d7508d3cee4a472d93b2d94380ad31472b07ce62
Allen Benny <allenbenny038@gmail.com>
2023-02-23 02:18:16 +0530

    Allen1

--- src/Header/TitleBar.js
+++ src/Header/TitleBar.js
@@ -4,9 +4,11 @@ import React from "react";
 const TitleBar = () => {
   return (
     <div className="TitleContainer">
-      <div>&#x0;</div> {/*Null Character */}
-      <div className="SearchBar">Search Bar</div>
+      <div>&#x0;</div>
+      <input className="SearchBar" placeholder="What do you want to buy today...?" />
       <div className="Registration">
+        <a href="www.google.com" target="_blank" id="earning">Total Earnings: &#8377;1550</a>
+        <button>How Does it work?</button>
         <button>LogIn</button>
         <button>SignIn</button>
       </div>
