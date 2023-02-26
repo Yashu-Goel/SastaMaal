3ad4a7266ab9f7fb3f739142815dcf3653273b30
Yashu <yashugoeldpsi@gmail.com>
2023-02-26 11:12:02 +0530

    Added Footer with Images 10

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,29 +1,57 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 import { SiLinkedin } from "react-icons/si";
 import { GoMarkGithub } from "react-icons/go";
-
-import YashuImg from './Yashu.png'
+import AllenImg from "./Allen.png"
+import YashuImg from "./Yashu.png";
 const Footer = () => {
   return (
-    <div className='FooterContainer'>
-        <footer>
-          <div className='Demo'>
-          <span>
-              Connect with us:
-            </span>
+    <div className="FooterContainer">
+      <footer>
+      <div className="Demo">
+          <span>Connect with us:</span>
           </div>
-            <div className='FooterImageContainer'>
-              <img src={YashuImg}/> 
-            </div>
-            <div className='SocialMedia'>
-              <a href='https://www.linkedin.com/in/yashu-goel/' target="_blank"><SiLinkedin/></a>
-              <a href='https://github.com/Yashu-Goel' target="_blank"><GoMarkGithub/></a>              
-            </div>
-            
-        </footer>
-    </div>
-  )
-}
+        <div className="OuterContainer">
+          <div>
+          <div className="FooterImageContainer">
+            <img src={AllenImg} />
+          </div>
+          <div className="NameContainer">
+          <p>Allen Benny</p>
+          </div>
 
-export default Footer
+          <div className="SocialMedia">
+            <a href="https://www.linkedin.com/in/allen-benny038/" target="_blank">
+              <SiLinkedin />
+            </a>
+            <a href="https://github.com/allenhack638" target="_blank">
+              <GoMarkGithub />
+            </a>
+          </div>
+          </div>
+          
+
+          <div>
+          <div className="FooterImageContainer">
+            <img src={YashuImg} />
+          </div>
+          <div className="NameContainer">
+          <p>Yashu Goel</p>
+          </div>
+          <div className="SocialMedia">
+            <a href="https://www.linkedin.com/in/yashu-goel/" target="_blank">
+              <SiLinkedin />
+            </a>
+            <a href="https://github.com/Yashu-Goel" target="_blank">
+              <GoMarkGithub />
+            </a>
+          </div>
+          </div>
+          
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
