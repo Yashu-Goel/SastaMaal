924d76b17117194649cad1f840f5647c5042e9bd
Allen Benny <allenbenny038@gmail.com>
2023-03-13 17:43:23 +0530

    final-backend

--- server/server.js
+++ server/server.js
@@ -17,20 +17,10 @@ import Offer_data from "./Controller/Data_routes/Offer_data.js"
 import TopCashBack from "./Controller/Data_routes/TopCashBack.js"
 import TopCategory from "./Controller/Data_routes/TopCategory.js"
 
-
 const app = express();
 dotenv.config();
 app.use(express.json());
-app.use(cors())
-
-mongoose.set("strictQuery", false);
-const url = process.env.CONNECTION_URL;
-mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log("Connected to database successfully"))
-    .catch(console.error)
+app.use(cors());
 
 app.get("/amount", get);
 app.post('/login', Login);
@@ -48,4 +38,14 @@ app.get('/TopCategoriesData', TopCategory);
 
 
 
-app.listen(5000, (console.log("Port has started at 5000")));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, (console.log("Port Successfully strated on port 5000")))
+
+mongoose.set("strictQuery", false);
+const url = process.env.CONNECTION_URL;
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("Connected to database successfully"))
+    .catch(console.error)
